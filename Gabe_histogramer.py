d404858feba6cb3e39c304ebd20e223219d9b650
Gabriel Brown <Gabey300@gmail.com>
2017-07-26 12:24:41 -0400

    Update Gabe_histogramer.py
    makes code more maintainable and normalizes the graphs now that the data-sets are different sizes.

--- Gabe_histogramer.py
+++ Gabe_histogramer.py
@@ -1,45 +1,32 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import csv
+import sys
 
 #parameter to modify
-read = 'antideuteron'
+read = sys.argv[1] #type of particle.... either antiproton or antideuteron
 
 if(read == 'antiproton'):
 	quants = list(csv.reader(open('pion quantities_pbar.txt', 'r'),delimiter = '\n'))
-	quants = [int(float(i[0])) for i in quants]
-	quants = np.asarray(quants)
-
-	average = np.mean(quants)
-	standard_deviation = np.std(quants)
-	print("average multiplicity of antiproton event = " + str(average))
-	print("standard deviation of set of antiproton events = " + str(standard_deviation))
-
-
-	plt.hist(quants, bins=range(0,20))
-
-	plt.xlabel('Pions produced per event')
-	plt.ylabel('Number of events')
-	plt.title('Histogram of Pion Multiplicity from Antiproton Event')
-	plt.axis([0,20,0,4000])
-	plt.grid(True)
-	plt.show()
-
-if(read =='antideuteron'):
+elif(read == 'antideuteron'):
 	quants = list(csv.reader(open('pion quantities_dbar.txt', 'r'),delimiter = '\n'))
-	quants = [int(float(i[0])) for i in quants]
-	quants = np.asarray(quants)
+else:
+	print("particle not recognized")
 
-	average = np.mean(quants)
-	standard_deviation = np.std(quants)
-	print("average multiplicity of antideuteron event = " + str(average))
-	print("standard deviation of set of antideuteron events = " + str(standard_deviation))
+quants = [int(float(i[0])) for i in quants]
+quants = np.asarray(quants)
 
-	plt.hist(quants, bins=range(0,20))
+average = np.mean(quants)
+standard_deviation = np.std(quants)
+print("average multiplicity of " + read + " event = " + str(average))
+print("standard deviation of set of "+read+" events = " + str(standard_deviation))
 
-	plt.xlabel('Pions produced per event')
-	plt.ylabel('Number of events')
-	plt.title('Histogram of Pion Multiplicity from Antideuteron Event')
-	plt.axis([0,20,0,4000])
-	plt.grid(True)
-	plt.show()
+
+plt.hist(quants, bins=range(0,20), normed=1)
+
+plt.xlabel('Pions produced per event')
+plt.ylabel('Number of events')
+plt.title('Histogram of Pion Multiplicity from '+read+' Event')
+plt.axis([0,20,0,1])
+plt.grid(True)
+plt.show()
